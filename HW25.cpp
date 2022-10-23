837654f3af3a46762e4d83873890251314c99af1
Nikita Sychev <nikitasychev@inbox.ru>
2022-10-23 12:14:02 +0500

    + add Task 2 ( palindron true or false )

--- HW25.cpp
+++ HW25.cpp
@@ -2,16 +2,26 @@
 #include <iostream>
 #include <string>
 
+bool is_palindrom(std::string str) {
+	int len = str.length();
+	for (int i = 0; i < len / 2; i++) {
+		if (str[i] != str[len - i - 1]) 
+			return false;
+		
+	}
+	return true;
+}
 
-
-
-
+void to_lower(std::string& str) {     
+	for (int i = 0; i < str.length();i++)
+		str[i] = tolower(str[i]);
+}
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 	int m, n;
 
-	std::cout << "Задание 1.\n";
+	std::cout << "Task 1.\n";
 	std::string str = "Hello, world!";
 	std::string sym = "o";
 	std::string new_str;
@@ -22,8 +32,16 @@ int main() {
 	std::cout << new_str << "\n";
 
 
+	std::cout << "Task 2.\n";
+	std::string palindrom;
+	std::cout << "Введите слово: \n";
+	std::cin >> palindrom;
+	to_lower(palindrom);
 	
-
+	if (is_palindrom(palindrom))
+		std::cout << "Введенное слово " << palindrom << " - палиндром" << ".\n";
+	else
+		std::cout << "Введенное слово " << palindrom << " не палиндром" << ".\n";
 
 
 
