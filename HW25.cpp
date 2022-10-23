271e47fd2b37c67aba4f7b5e619a8272bcdb21c0
Nikita Sychev <nikitasychev@inbox.ru>
2022-10-23 11:18:08 +0500

    Task1

--- HW25.cpp
+++ HW25.cpp
@@ -1,13 +1,28 @@
 //Этот проект создан с помощью классического шаблона
 #include <iostream>
+#include <string>
+
+
+
+
+
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 	int m, n;
 
+	std::cout << "Задание 1.\n";
+	std::string str = "Hello, world!";
+	std::string sym = "o";
+	std::string new_str;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == 'o')
+			new_str += sym;
+	}
+	std::cout << new_str << "\n";
 
 
-
+	
 
 
 
