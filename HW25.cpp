f6941ab632c4a6bce34b7818861cdcdccf9d7cf5
Nikita Sychev <nikitasychev@inbox.ru>
2022-10-24 11:53:20 +0500

    Task 3 пока не корректно считает символы, добавил getline()

--- HW25.cpp
+++ HW25.cpp
@@ -17,6 +17,18 @@ void to_lower(std::string& str) {
 		str[i] = tolower(str[i]);
 }
 
+int count_sym(std::string str) {
+	int counter = 0;
+	
+	
+	getline(std::cin, str);
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == ' ' || str[i] == '.' || str[i] == ',' || str[i] == '!' || str[i] == '?')
+			counter++;
+	}
+	return counter;
+}
+
 int main() {
 	setlocale(LC_ALL, "Russian");
 	int m, n;
@@ -42,6 +54,16 @@ int main() {
 		std::cout << "Введенное слово " << palindrom << " - палиндром" << ".\n";
 	else
 		std::cout << "Введенное слово " << palindrom << " не палиндром" << ".\n";
+
+
+	std::cout << "Tasl 3.\n";
+	std::string str1;
+	std:: cout << "Введите фразу с символами: ";
+	
+	std::cin >> str1;
+	
+	std::cout << "Количество не буквенных символов в фразе равно: " << count_sym(str1) << ".\n";
+
 
 
 
